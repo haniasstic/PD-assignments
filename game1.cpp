ca15a85077be562f6df70868ee5da40dc9155df0
haniasstic <haniabukhari1040@gmail.com>
2024-09-14 12:40:51 +0500

    Add files via upload

---
+++ game1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+ cout << "                  . : : _ _ _ : : .                    " << endl;
+ cout << "             . _ _ _ _ _ _ _ _ _ _ .                  " <<  endl;
+ cout << "            . _ _ _ _ _ _ _ _ _ _ _ _ .                   " << endl;
+ cout << "           _ _ _ _ _ _ _ _ _ _ _ _ _ _ : .                      " << endl;
+ cout << "         : _ _ _ _ _ _ _ _ _ _ _ _ : : .                     " << endl;
+ cout << "          _ _ _ _ _ _ _ _ _ _ _ : .                       " << endl;
+ cout << "          : _ _ _ _ _ _ _ _ _ _ _ _  _ : .           " << endl;
+ cout << "            : _ _ _ _ _ _ _ _ _ _ _ _ : .           " << endl;
+ cout << "              . _ _ _ _ _ _ _ _ _ _ .              " << endl;
+ cout << "                  . _ _ _ _ _ _ _ .                  " << endl;
+ return 0;
+}
