e37b14b271d6760da8767af059dcb00ab2d460aa
haniasstic <haniabukhari1040@gmail.com>
2024-09-15 16:25:36 +0500

    Add files via upload

---
+++ game1 color.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	system("color 24");
+ cout << "                  . : : _ _ _ : : .                    " << endl;
+ cout << "             . _ _ _ _ _ _ _ _ _ _ .                  " <<  endl;
+ cout << "            . _ _ _ _ _ _ _ _ _ _ _ _ .                   " << endl;
+ cout << "           _ _ _ _ _ _ _ _ _ _ _ _ _ _ : .                      " << endl;
+ cout << "         : _ _ _ _ _ _ _ _ _ _ _ _ : : .                     " << endl;
+ cout << "          _ _ _ _ _ _ _ _ _ _ _ : .                       " << endl;
+ cout << "          : _ _ _ _ _ _ _ _ _ _ _ _  _ : .           " << endl;
+ cout << "            : _ _ _ _ _ _ _ _ _ _ _ _ : .           " << endl;
+ cout << "              . _ _ _ _ _ _ _ _ _ _ .              " << endl;
+ cout << "                  . _ _ _ _ _ _ _ .                  " << endl;
+ return 0;
+}
