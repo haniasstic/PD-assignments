1515782dc3e21fcc25508169a50ab98c66338f7c
haniasstic <haniabukhari1040@gmail.com>
2024-09-15 19:13:58 +0500

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout << "                +_ _ _ ^ _ _ _ _ _ _ _ _ _ , _ _ _ _ _ , _ _ _ _ _ , _ _ _ _ _ _^ _,             " << endl;
+cout << "                  |      ||||||||||||       , _ _ _ _ _ _ ,                |                   o,           " << endl;
+cout << "                   '       +_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ^ _ _ _ _ _ _ |           " << endl;
+cout << "                        ' ,    _ _ _ _ _ _ _ _ , _ _ _ _ _ _ _ , _ _ _ _ _ _ _ _ _ _ _ _',             " << endl; 
+cout << "                        /  xxxxx      /  ' |                   /'                                                          " << endl;
+cout << "                     /    xxxxx     /     |                 /'                                                             " << endl;
+cout << "                  /     xxxxx     /  '  _ _ _ _ _ _  '                                                                 " << endl;
+cout << "               /    xxxxxxx    /                                                                                           " << endl;
+cout << "            /      xxxxxxx   /                                                                                             " << endl;
+cout << "           ( _ _ _ _ _ _ _ _(                                                                                            " << endl;
+cout << "             ' _ _ _ _ _ _ '                                                                                                " << endl;
+cout << "                                                                                                                                 " << endl;
+return 0;
+}
