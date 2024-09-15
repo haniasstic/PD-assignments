630c543836acd952a3f8ca1101142e9116dc0632
haniasstic <haniabukhari1040@gmail.com>
2024-09-15 17:34:48 +0500

    Add files via upload

---
+++ donkey.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ cout << "    _ _ _ _ _ _ _ _ _ _ _ _ _ _        " << endl;
+ cout << "     o            ^        ^                      " << endl;
+ cout << "                   ______                      " << endl;
+ cout << "         o         ( o o ) \______         " << endl;
+ cout << "                                                     " << endl;
+ cout << "                    (____) \      )\/ \      " << endl;
+ cout << "                       | | _ _ _w     |      " << endl;
+ cout << "                       | |              | |      " << endl;
+return 0;
+}
